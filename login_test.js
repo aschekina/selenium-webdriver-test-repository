0d9bcdfa3b8d0345e0810d78fbb12395c51b6615
aschekina <72140567+aschekina@users.noreply.github.com>
2021-04-01 14:28:31 +0300

    Create login_test.js

---
+++ login_test.js
@@ -0,0 +1,25 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until,
+    test = require('selenium-webdriver/testing');
+
+test.describe('Логин в приложении Litecart', function() {
+    var driver;
+
+    test.before(function() {
+        driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .build();
+    });
+
+    test.it('Вводи логин и пароль, нажимаем кнопку', function() {
+        driver.get('http://localhost/litecart/admin');
+        driver.findElement(By.name('username')).sendKeys('admin');
+        driver.findElement(By.name('password')).sendKeys('admin');
+        driver.findElement(By.name('login')).click()
+    });
+
+    test.after(function() {
+        driver.quit();
+    });
+});
